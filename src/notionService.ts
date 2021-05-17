dcd49d13fea6ec48d1e3599547d1e0f679223abe
Audrey <aliv074@aucklanduni.ac.nz>
2021-05-17 20:50:50 +1200

    Added another property onto service

--- src/notionService.ts
+++ src/notionService.ts
@@ -1,4 +1,4 @@
-import { Database, Page } from "./notionTypes";
+import { Block, Database, Page, PaginatedList } from "./notionTypes";
 
 import { Client } from "@notionhq/client";
 import { config } from "dotenv";
@@ -7,6 +7,7 @@ config();
 
 const notionPages = {
     today: "24dffb3bccac430c912a94db6b10aecc",
+    dailyBoard: "a311538a-d0d1-4f43-aafb-1f4afda58692",
 };
 
 const notion = new Client({
@@ -25,7 +26,17 @@ const useRetrieveNotionDatabase = async (dbId: string): Promise<Database> => {
     });
 };
 
+const useRetrieveNotionBlockChildren = async (
+    blockId: string
+): Promise<PaginatedList<Block>> => {
+    return await notion.blocks.children.list({
+        block_id: blockId,
+    });
+};
+
 (async () => {
-    const dailyBoard = await useRetrieveNotionPage(notionPages.today);
-    console.log(dailyBoard);
+    const dailyBoard = await useRetrieveNotionBlockChildren(
+        notionPages.dailyBoard
+    );
+    console.log(dailyBoard.results);
 })();
