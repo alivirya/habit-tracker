57a682ccbe0e1df4f298a88bb964945d28a546bf
Audrey <aliv074@aucklanduni.ac.nz>
2021-06-13 20:59:05 +1200

    This is me pivoting to create a habit tracker instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,45 @@
 import "./style.css";
 
-import { CurrentMedia } from "./CurrentMedia";
-import React from "react";
+import React, { Fragment } from "react";
+
 import { render } from "react-dom";
 
 const App = () => {
     return (
         <React.StrictMode>
             <div className="app">
-                <CurrentMedia />
+                <Tracker />
             </div>
         </React.StrictMode>
+    );
+};
+
+const Tracker = () => {
+    return (
+        <div className="tracker">
+            <DayCheckBox day="Monday" />
+            <DayCheckBox day="Tuesday" />
+            <DayCheckBox day="Wednesday" />
+            <DayCheckBox day="Thursday" />
+            <DayCheckBox day="Friday" />
+            <DayCheckBox day="Saturday" />
+            <DayCheckBox day="Sunday" />
+        </div>
+    );
+};
+
+export interface DayCheckBoxProps {
+    day: string;
+}
+
+const DayCheckBox = ({ day }: DayCheckBoxProps) => {
+    return (
+        <div className="dailyCheckBoxContainer">
+            <label htmlFor={day} className="dailyCheckboxLabel">
+                {day}
+            </label>
+            <input type="checkbox" name={day} className="dailyCheckBox" />
+        </div>
     );
 };
 
