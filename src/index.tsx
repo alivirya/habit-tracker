f29a995c181100a44d60383b089373415be7f309
Audrey Livirya <audrey.livirya@xero.com>
2021-10-03 19:20:14 +1300

    Added default wallpaper

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,10 @@ const App = (): ReactElement => {
         chrome.storage.local.get(
             "background",
             ({ background }: { [key: string]: string }) => {
-                setBackground(background);
+                setBackground(
+                    background ||
+                        "https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80"
+                );
             }
         );
     }, []);
